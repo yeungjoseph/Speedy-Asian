0ff5db596a1eefced8afa9c04563461b791bb8e1
Joseph Yeung <yeungjoseph10@gmail.com>
2017-10-23 00:23:47 -0700

    Add basic 3D infinite runner template
    Includes infinite path generation, path auto destruction, and control-less player running in straight line

---
+++ InfiniteRunner2/Assets/Scripts/RoadTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadTrigger : MonoBehaviour {
+    public Transform roadPrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Create a new road at the position of the last road + length of a road
+        Instantiate(roadPrefab, new Vector3(0, 0, transform.parent.position.z + 154.71f), roadPrefab.rotation);
+        transform.parent.gameObject.AddComponent<TimeToDestroy>();
+    }
+}
---
+++ InfiniteRunner2/Assets/Scripts/Run.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Run : MonoBehaviour {
+    private Rigidbody thisrigid;
+    public float speed = 5f;
+
+	// Use this for initialization
+	void Start () {
+        thisrigid = GetComponent<Rigidbody>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        thisrigid.velocity = new Vector3(0, thisrigid.velocity.y, speed);
+	}
+}
