e2891aab2bcb24f2bec7d32334b623893198d43d
Joseph Yeung <yeungjoseph10@gmail.com>
2017-10-24 22:41:31 -0700

    removed excess files

---
+++ InfiniteRunner1/Assets/Scripts/WaveTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveTrigger : MonoBehaviour {
+    public Transform wavePrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Transform wave = Instantiate(wavePrefab, new Vector3(transform.parent.position.x, transform.parent.position.y, 
+            transform.parent.position.z + 224f), wavePrefab.rotation);
+        wave.name = "wave";
+
+        transform.parent.gameObject.AddComponent<TimeToDestroy>();
+    }
+}
